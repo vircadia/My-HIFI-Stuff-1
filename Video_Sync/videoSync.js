14d8a810100bd11dbf069f6d0222416dceabee58
Darlingnotin <darlingnotin@hotmail.com>
2019-10-02 06:41:50 -0400

    Update videoSync.js

--- Video_Sync/videoSync.js
+++ Video_Sync/videoSync.js
@@ -1,12 +1,14 @@
 (function () {
     var uuid;
     var script = this;
+    var pause = "og";
     var sourceUrl = Script.resolvePath("videoSync.html");
     script.preload = function (entityID) {
         var entity = Entities.getEntityProperties(entityID, ["position", "rotation", "dimensions"]);
         uuid = Entities.addEntity({
             type: "Web",
             dpi: 15,
+            maxFPS: 60,
             sourceUrl: sourceUrl,
             rotation: entity.rotation,
             dimensions: {
@@ -23,25 +25,42 @@
         Entities.deleteEntity(uuid);
     });
 
-function webEvent() {
-    Entities.webEventReceived.connect(function (uuid, event) {
-        messageData = JSON.parse(event);
-        print("timeStamp " + messageData.timeStamp);
-        print("action " + messageData.action);
-        print("videoUrl " + messageData.videoUrl);
-        print("now video " + messageData.nowVideo);
-        Messages.sendMessage("videoPlayOnEntity", event);
-    })
-}
-
-function onMessageReceived(channel, message, sender, localOnly) {
-    if (channel != "videoPlayOnEntity") {
-        return;
+    function webEvent() {
+        Entities.webEventReceived.connect(function (uuid, event) {
+            messageData = JSON.parse(event);
+            if (pause == "stop") {
+                print("Event is paused");
+            } else {
+                pause = "stop";
+                stopPausEvent();
+                print("timeStamp " + messageData.timeStamp);
+                print("action " + messageData.action);
+                print("videoUrl " + messageData.videoUrl);
+                print("now video " + messageData.nowVideo);
+                Messages.sendMessage("videoPlayOnEntity", event);
+            }
+        })
     }
-    Entities.emitScriptEvent(uuid, message);
-}
 
-Messages.subscribe("videoPlayOnEntity");
-Messages.messageReceived.connect(onMessageReceived);
+    function stopPausEvent() {
+        Script.setTimeout(function () {
+            pause = "og";
+            print("go");
+        }, 500);
+    }
+
+    function onMessageReceived(channel, message, sender, localOnly) {
+        if (channel != "videoPlayOnEntity") {
+            return;
+        }
+        if (pause == "stop") {
+            print("Event is paused");
+        } else {
+            Entities.emitScriptEvent(uuid, message);
+        }
+    }
+
+    Messages.subscribe("videoPlayOnEntity");
+    Messages.messageReceived.connect(onMessageReceived);
 
 });
