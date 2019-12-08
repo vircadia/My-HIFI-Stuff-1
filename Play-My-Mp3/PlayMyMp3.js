ef66539a82e5d00fae39d8d781d34ed0205c28ae
Darlingnotin <darlingnotin@hotmail.com>
2019-12-08 09:33:58 -0500

    Update PlayMyMp3.js

--- Play-My-Mp3/PlayMyMp3.js
+++ Play-My-Mp3/PlayMyMp3.js
@@ -1,13 +1,18 @@
 (function () {
+    var speakerUrl = Script.resolvePath("speaker.fbx");
+    var speakerInUse = "no";
+    var entityID;
     var injectorIsRunning = "no";
     var tablet = Tablet.getTablet("com.highfidelity.interface.tablet.system");
     var AppUi = Script.require('appUi');
     var sound;
+    var use = "avatar";
     var injectorOptions = {
         position: MyAvatar.position,
         volume: 0.01
     };
     var injector;
+    var soundPlayingAt = "avatar";
     startup();
     var ui;
     function startup() {
@@ -20,8 +25,10 @@
 
     function browseForMp3() {
         var filename = Window.browse("Select mp3", "");
-        sound = SoundCache.getSound(filename);
-        okGo(sound);
+        if (filename != null) {
+            sound = SoundCache.getSound(filename);
+            okGo(sound);
+        }
     }
 
     function playSound() {
@@ -38,7 +45,13 @@
         if (injectorIsRunning == "yes") {
             injector.stop();
         }
-        injectorOptions.position = MyAvatar.position;
+        if (use == "avatar") {
+            var thePositionAt = MyAvatar.position;
+        } else if (use == "speaker") {
+            entity = Entities.getEntityProperties(entityID, ["position"]);
+            var thePositionAt = entity.position;
+        }
+        injectorOptions.position = thePositionAt;
         if (sound.downloaded) {
             playSound();
         } else {
@@ -60,16 +73,48 @@
         } else if (messageData.action == "stopIt") {
             injector.stop();
             injectorIsRunning = "no";
+        } else if (messageData.action == "buttonStatus") {
+            var readyEvent = {
+                "action": "buttonStatusResponse",
+                "soundPlayingAt": soundPlayingAt
+            };
+            tablet.emitScriptEvent(JSON.stringify(readyEvent));
+        } else if (messageData.action == "soundPlayAt") {
+            if (messageData.soundPlayAt == "speaker") {
+                use = "speaker";
+                soundPlayingAt = "speaker";
+                entityID = Entities.addEntity({
+                    type: "Model",
+                    modelURL: speakerUrl,
+                    position: Vec3.sum(MyAvatar.position, Quat.getFront(MyAvatar.orientation))
+                }, "avatar");
+                speakerInUse = "yes";
+            } else if (messageData.soundPlayAt == "avatar") {
+                use = "avatar";
+                soundPlayingAt = "avatar";
+                if (speakerInUse == "yes") {
+                    Entities.deleteEntity(entityID);
+                    speakerInUse = "no";
+                }
+            }
         }
     }
 
     MyAvatar.sessionUUIDChanged.connect(function () {
         injector.stop();
+        use = avatar;
+        if (speakerInUse == "yes") {
+            Entities.deleteEntity(entityID);
+            speakerInUse = "no";
+        }
     });
 
     tablet.webEventReceived.connect(onWebEventReceived);
 
     Script.scriptEnding.connect(function () {
         tablet.webEventReceived.disconnect(onWebEventReceived);
+        if (speakerInUse == "yes") {
+            Entities.deleteEntity(entityID);
+        }
     });
 }());
