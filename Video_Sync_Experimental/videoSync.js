44bc6c786bd6a1e8f89f18cd328ca6ad687ae93c
Darlingnotin <darlingnotin@hotmail.com>
2020-08-19 17:29:05 -0400

    Update videoSync.js

--- Video_Sync_Experimental/videoSync.js
+++ Video_Sync_Experimental/videoSync.js
@@ -70,14 +70,20 @@
             if (messageData.uuid == playButtonUuid || messageData.uuid == pauseButtonUuid) {
                 sendMessage(message);
             }
-        }else if (messageData.action == "requestVideoPlayingStatusReply") {
+        } else if (messageData.action == "requestVideoPlayingStatusReply") {
             sendMessage(message);
         } else if (messageData.action == "volumeButton") {
             if (messageData.uuid == volumeButtonMinus || messageData.uuid == volumeButtonPlus) {
                 sendMessage(message);
             }
         } else if (!hasBeenSynced) {
-            if (messageData.action == "sync" || messageData.action == "now") {
+            if (messageData.action == "sync" && messageData.action != "now") {
+                if (messageData.myTimeStamp == HtmlTimeStamp) {
+                    sendMessage(message);
+                    hasBeenSynced = true;
+                    makeControlButtonsVisible();
+                }
+            } else if (messageData.action == "now" && hasBeenSynced) {
                 sendMessage(message);
                 hasBeenSynced = true;
                 makeControlButtonsVisible();
@@ -91,7 +97,7 @@
 
     function makeControlButtonsVisible() {
         if (Entities.canRez() == true) {
-            
+
             Entities.editEntity(videoInterfaceButton, {
                 visible: true,
                 script: videoSyncInterface,
@@ -99,7 +105,7 @@
                     "Button": "pause"
                 })
             });
-            
+
             Entities.editEntity(playButtonUuid, {
                 visible: true,
                 script: playPauseButtonUrl,
@@ -115,7 +121,7 @@
                     "Button": "pause"
                 })
             });
-            
+
         }
     }
 
@@ -138,7 +144,7 @@
                     "grabbable": false,
                 }
             }, "local");
-            
+
             playButtonUuid = Entities.addEntity({
                 type: "Model",
                 modelURL: playButtonFbxUrl,
@@ -158,7 +164,7 @@
                     "grabbable": false,
                 }
             }, "local");
-    
+
             pauseButtonUuid = Entities.addEntity({
                 type: "Model",
                 modelURL: pauseButtonURL,
@@ -220,11 +226,11 @@
                 "grabbable": false,
             }
         }, "local");
-        
+
         Entities.editEntity(uuid, {
             rotation: entity.rotation
         });
-        
+
     }
 
     function sendMessage(message) {
@@ -252,9 +258,9 @@
                 Entities.editEntity(uuid, {
                     rotation: originalRotation,
                 });
-                
+
                 if (Entities.canRez() == true) {
-                    
+
                     Entities.editEntity(videoInterfaceButton, {
                         position: {
                             "x": entity.position.x - entity.dimensions.x / 2 - -0.5,
@@ -262,7 +268,7 @@
                             "z": entity.position.z
                         },
                     });
-                
+
                     Entities.editEntity(playButtonUuid, {
                         position: {
                             "x": entity.position.x - entity.dimensions.x / 2 - -0.2,
@@ -270,7 +276,7 @@
                             "z": entity.position.z
                         }
                     });
-    
+
                     Entities.editEntity(pauseButtonUuid, {
                         position: {
                             "x": entity.position.x - entity.dimensions.x / 2 - -0.5,
@@ -278,7 +284,7 @@
                             "z": entity.position.z
                         }
                     });
-                    
+
                 }
 
                 Entities.editEntity(volumeButtonMinus, {
